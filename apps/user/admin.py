db377bfbf5a1f4a9701bebfbad1f84be4d02cf06
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-03-01 11:43:27 +0330

    feat: admin compatible with user model

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    model = models.User  # type: ignore
+    ordering = ("email",)
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_superuser",
+    )
+    readonly_fields = ("uuid",)
+    fieldsets = (  # type: ignore
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "uuid")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
