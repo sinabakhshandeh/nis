c177d144a237a139034cb67d7649a6484d975576
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-03-01 11:42:01 +0330

    feat: define user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .maganers import CustomUserManager
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()  # type:ignore
+
+    username = None  # type: ignore
+    email = models.EmailField(_("email address"), unique=True)
+    phone_number = models.CharField(
+        _("Phone number"), max_length=150, blank=True, null=True
+    )
+    description = models.CharField(max_length=255, null=True, blank=True)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
