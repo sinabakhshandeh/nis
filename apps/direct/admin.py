edd400d644471515483874d910f6742acf6562da
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-03-02 19:53:01 +0330

    update(direct): add model to admin

--- apps/direct/admin.py
+++ apps/direct/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DirectChat, DirectMessage
+
+
+class DirectChatAdmin(admin.ModelAdmin):
+    list_display = ("id", "__str__")
+    filter_horizontal = ("participants",)
+    search_fields = ["participants__username"]
+
+
+class DirectMessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "sender", "chat", "sent_at")
+    list_filter = ("chat",)
+    search_fields = ["sender__username", "content"]
+
+
+admin.site.register(DirectChat, DirectChatAdmin)
+admin.site.register(DirectMessage, DirectMessageAdmin)
