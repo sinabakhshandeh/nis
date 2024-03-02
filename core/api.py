e336e5f4bf771c7db4bd40d5a3c2055d4ec88da8
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-03-02 18:11:58 +0330

    update(user): api middlewares update

--- core/api.py
+++ core/api.py
@@ -2,7 +2,8 @@ from ninja import NinjaAPI
 
 from apps.user.api.views import router as user_router
 
+from .middlewares import AuthBearer, BasicAuth
 
-api = NinjaAPI()
+api = NinjaAPI(auth=[AuthBearer(), BasicAuth()])
 
 api.add_router("/auth/", user_router, tags=["Auth"])
